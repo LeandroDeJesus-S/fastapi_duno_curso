987953be8884c2f282a05695b44361d1c1892a3c
LeandroDeJesus-S <jstleandro@gmail.com>
2024-07-16 05:11:29 -0300

    feat: add read_html

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+from .schemas import Message
 
 api = FastAPI()
 
 
-@api.get('/')
+@api.get('/', response_model=Message)
 def read_root():
     return {'message': 'Olá Mundo!'}
+
+
+@api.get('/html', response_class=HTMLResponse)
+def read_html():
+    return '<h1>Olá Mundo!</h1>'
--- tests/test_app.py
+++ tests/test_app.py
@@ -10,3 +10,10 @@ def test_root_deve_retornar_ok_e_ola_mundo():
     response = client.get('/')
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Olá Mundo!'}
+
+
+def test_read_html_deve_retornar_html():
+    client = TestClient(api)
+    response = client.get('/html')
+    assert response.status_code == HTTPStatus.OK
+    assert response.text == '<h1>Olá Mundo!</h1>'
